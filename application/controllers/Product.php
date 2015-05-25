1bce52354023b571338dca96138e059f1426d908
z32556601 <zhaojiangwei6464@163.com>
2015-05-25 18:17:42 +0800

    fix

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -4,8 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Product extends CI_Controller {
     
     public function index(){
-        $data = array();
-        $this->load->view('product.php', $data);
+        if(check_device()){
+            $data = array();
+            $this->load->view('wechat.php', $data);
+        }else{
+            $data = array();
+            $this->load->view('product.php', $data);
+        }
     }
 
     public function config_detail($type = 1){
