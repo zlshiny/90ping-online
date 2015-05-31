464f37f2780cbd5d36a9758031c68a3416e642d4
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com>
2015-05-31 17:26:55 +0800

    fix

--- application/views/mobile/order_list.php
+++ application/views/mobile/order_list.php
@@ -1,13 +1,13 @@
 <?php  require_once 'header.php'; ?>
 <script type="text/javascript">
     $(function(){
-        $('#arrow-img').on('click',function(){
-            var display = $('#my-order-content').css('display');
+        $('.arrow-img').on('click',function(){
+            var display = $(this).parent().parent().next(".my-order-content").css('display');
             if(display == 'none'){
-                $('#my-order-content').show();
+                $(this).parent().parent().next(".my-order-content").show();
                 $(this).attr('src','/static/image/mobile/arrow-up.png');
             }else{
-                $('#my-order-content').hide();
+                $(this).parent().parent().next(".my-order-content").hide();
                 $(this).attr('src','/static/image/mobile/arrow-down.png');
             }
         })
@@ -31,10 +31,10 @@
             <div class="order-menu">
                 <div class="order-num">订单编号：<?=$v['serial_number'];?></div>
                 <div class="arrow">
-                    <img id="arrow-img" src="/static/image/mobile/arrow-up.png">
+                    <img class="arrow-img" src="/static/image/mobile/arrow-up.png">
                 </div>
             </div>
-            <div class="my-order-content" id="my-order-content">
+            <div class="my-order-content">
                 <p>姓名&nbsp;&nbsp;<?=$v['name'];?></p>
                 <p>产品&nbsp;&nbsp;<?=$v['product_name'];?></p>
                 <p>定金&nbsp;&nbsp;<?=$v['init_deposit'];?>元</p>
@@ -43,7 +43,7 @@
                     <p>操作</p>
                     <?php if($v['status'] > 0):?><a href="/order/detail/<?=$v['order_id'];?>" class="my-order-a">查看</a><?php endif;?>
                     <?php if($v['status'] == 0):?>
-                        <a href="#" class="my-order-a" id="action_order" serial-number="<?=$v['serial_number'];?>" order-id="<?=$v['order_id'];?>" user-id="<?=$v['user_id'];?>" value="<?=$v['status'];?>">
+                        <a href="#" class="my-order-a" serial-number="<?=$v['serial_number'];?>" order-id="<?=$v['order_id'];?>" user-id="<?=$v['user_id'];?>" value="<?=$v['status'];?>">
                             <?=$v['action_name'];?>
                         </a>
                     <?php endif;?>
