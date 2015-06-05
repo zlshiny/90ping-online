45ea791e389ceeb79b1333fb62048a0342e35f86
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com>
2015-06-05 14:55:58 +0800

    add negibor activity

---
+++ application/views/activity/detail.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<!--HTML5 doctype-->
+<html>
+<head>
+    <title>home1.2</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta HTTP-EQUIV="Pragma" CONTENT="no-cache">
+    <meta content="telephone=no" name="format-detection" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" type="text/css" href="css/common.css" />
+    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/fastclick.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/jquery.bxslider.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/common.js"></script>   
+</head>
+
+<body class="detail">
+
+<div class="detail-title">
+    <img src="image/detail-title.png">
+</div>
+
+<div class="detail-area">
+    <div class="detail-banner">
+        <div class="button-area">
+            <div class="button">
+                <button type="button" class="detail-banner-button ">我要参与</button>
+            </div>
+            <div class="button">
+                <button type="button" class="detail-banner-button ">我要发起</button>
+            </div>
+        </div>
+        <p>结束时间：还剩4天22时15分56秒</p>
+    </div>
+
+    <div class="detail-content">
+
+        <div class="desciption">
+            <div class="desciption-title">
+                <p class="p1">众筹优惠目标：<font class="p1-money">20万</font></p>
+                <p class="p2">满20人，每人优惠10000元</p>
+            </div>
+            <div class="author">
+                发起人：赵将伟
+            </div>
+            <div class="desciption-content">
+                无论70、80、还是90后，只需三步，你的肌肤蜕变00后！彩虹（Rainbow）三部曲胶原蛋白弹力面膜贴10片/盒！首创面膜三部曲：Step1深层洁面，打开肌肤通道；Step2安瓶精华镇定肌肤促进后续吸收；Step3美白保湿，给肌肤注入胶原蛋白新能量！让时间继续，让颜值状态永远18岁！
+            </div>
+        </div>
+
+        <div class="content-menu">
+            <div class="content-menu-sub">
+                <div class="content-menu-title">
+                    当前参与人次
+                </div>
+                <div class="content-menu-content">
+                    5 人
+                </div>
+            </div>
+
+            <div class="content-menu-sub">
+                <div class="content-menu-title">
+                    距目标还差
+                </div>
+                <div class="content-menu-content">
+                    3 人
+                </div>
+            </div>
+
+            <div class="content-menu-sub">
+                <div class="content-menu-title">
+                    发起时间
+                </div>
+                <div class="content-menu-content">
+                    5.29
+                </div>
+            </div>
+
+        </div>
+
+        <div class="user-list">
+            <p>参与用户</p>
+            <table class="user-list-table">
+                <tr>
+                    <td class="center" width="35%">张三</td>
+                    <td class="pleft" width="30%">1504</td>
+                    <td class="pleft" width="35%">5-23 12：19</td>
+                </tr>
+                <tr>
+                    <td class="center">张三</td>
+                    <td class="pleft">1504</td>
+                    <td class="pleft">5-23 12：19</td>
+                </tr>
+                <tr>
+                    <td class="center">张三</td>
+                    <td class="pleft">1504</td>
+                    <td class="pleft">5-23 12：19</td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="share">
+            <p>分享到</p>
+            <div class="weibo">
+                <a href="#"><img src="image/weibo.png"></a>
+            </div>
+            <div class="weixin">
+                <a href="#"><img src="image/weixin.png"></a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</body>
+</html>
---
+++ application/views/activity/zc.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--HTML5 doctype-->
+<html>
+<head>
+    <title>home1.2</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta HTTP-EQUIV="Pragma" CONTENT="no-cache">
+    <meta content="telephone=no" name="format-detection" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" type="text/css" href="css/common.css" />
+    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/fastclick.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/jquery.bxslider.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/common.js"></script>   
+</head>
+
+<body class="zc">
+
+    <div class="start">
+        <button type="button" class="zc-start-button ">我要发起</button>
+    </div>
+
+    <div class="info-list">
+        <p>机制</p>
+        <ul>
+            <li>用户可以在小区发起团购，发起者立减1000元</li>
+            <li>同小区凑齐5户预约，每个用户超级Home1.2价格变为16万。（各省2000元）；</li>
+            <li>同小区凑齐10户交定金，返5000元现金。</li>
+            <li>同小区凑齐20户交定金，每户返1万现金。</li>
+        </ul>
+    </div>
+
+    <div class="info-list">
+        <p>规则</p>
+        <ul>
+            <li>参与“众筹优惠”活动用户，以交定金的人数为准，定金金额为2000元人民币。</li>
+            <li>同一个“众筹优惠”活动的用户，必须为同一小区同一期交房的业主。</li>
+            <li>参与“众筹优惠”用户，在合同签订付款时，享受优惠立减。</li>
+        </ul>
+    </div>
+
+    <div class="privilege">
+        <div class="privilege-area">
+            <div class="title">
+                <p class="p1">和谐小区</p>
+                <p class="p2">北京市海淀区回龙观</p>
+            </div>
+            <div class="privilege-content">
+                <p>发起人：赵将伟</p>
+                <p>当前参与人数：3</p>
+                <p>当前优惠金额：2000</p>
+                <p>距离优惠5000还剩：2人</p>
+            </div>
+        </div>
+        <button type="button" class="privilege-button ">我要优惠</button>
+    </div>
+
+</body>
+</html>
