550b0e9a3c4ce3a114784c04b4b4b5d306d50abe
z32556601 <zhaojiangwei6464@163.com>
2015-06-26 14:51:25 +0800

    fix

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -6,7 +6,7 @@ class Product extends CI_Controller {
     public function index(){
         if(check_device()){
             $data = array();
-            $this->load->view('wechat.php', $data);
+            $this->load->view('h5_v2.php');
         }else{
             $data = array();
             $this->load->view('product2.php', $data);
@@ -15,7 +15,13 @@ class Product extends CI_Controller {
 
     public function v2(){
         $data = array();
-        $this->load->view('product2.php', $data);
+        if(check_device()){
+            $data = array();
+            $this->load->view('h5_v2.php');
+        }else{
+            $data = array();
+            $this->load->view('product2.php', $data);
+        }
     }
 
     public function config_detail($type = 1){
